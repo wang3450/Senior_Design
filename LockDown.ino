049bc972ea3f4531e254fcc978a7221619397b51
unknown <wang3450@purdue.edu>
2020-10-16 21:14:31 -0400

    Working Advertisement, Connect,Service, Characteristics, Descriptor

---
+++ LockDown.ino
@@ -0,0 +1,126 @@
+/** BLE Server
+ *  Written By Joseph Wang
+*/
+
+#include <NimBLEDevice.h>
+
+static NimBLEServer* pServer;
+
+/**  None of these are required as they will be handled by the library with defaults. **
+ **                       Remove as you see fit for your needs                        */  
+class ServerCallbacks: public NimBLEServerCallbacks {
+    void onConnect(NimBLEServer* pServer) {
+        Serial.println("Client connected");
+        Serial.println("Multi-connect support: start advertising");
+        NimBLEDevice::startAdvertising();
+    };
+    /** Alternative onConnect() method to extract details of the connection. 
+     *  See: src/ble_gap.h for the details of the ble_gap_conn_desc struct.
+     */  
+    void onConnect(NimBLEServer* pServer, ble_gap_conn_desc* desc) {
+        Serial.print("Client address: ");
+        Serial.println(NimBLEAddress(desc->peer_ota_addr).toString().c_str());
+        /** We can use the connection handle here to ask for different connection parameters.
+         *  Args: connection handle, min connection interval, max connection interval
+         *  latency, supervision timeout.
+         *  Units; Min/Max Intervals: 1.25 millisecond increments.
+         *  Latency: number of intervals allowed to skip.
+         *  Timeout: 10 millisecond increments, try for 5x interval time for best results.  
+         */
+        pServer->updateConnParams(desc->conn_handle, 24, 48, 0, 60);
+    };
+};
+
+/** Handler class for characteristic actions */
+class CharacteristicCallbacks: public NimBLECharacteristicCallbacks {
+    void onRead(NimBLECharacteristic* pCharacteristic){
+        Serial.print(pCharacteristic->getUUID().toString().c_str());
+        Serial.print(": onRead(), value: ");
+        Serial.println(pCharacteristic->getValue().c_str());
+    };
+
+    void onWrite(NimBLECharacteristic* pCharacteristic) {
+        Serial.print(pCharacteristic->getUUID().toString().c_str());
+        Serial.print(": onWrite(), value: ");
+        Serial.println(pCharacteristic->getValue().c_str());
+    };
+    /** Called before notification or indication is sent, 
+     *  the value can be changed here before sending if desired.
+     */
+    void onNotify(NimBLECharacteristic* pCharacteristic) {
+        Serial.println("Sending notification to clients");
+    };
+};
+    
+/** Handler class for descriptor actions */    
+class DescriptorCallbacks : public NimBLEDescriptorCallbacks {
+    void onWrite(NimBLEDescriptor* pDescriptor) {
+        std::string dscVal((char*)pDescriptor->getValue(), pDescriptor->getLength());
+        Serial.print("Descriptor witten value:");
+        Serial.println(dscVal.c_str());
+    };
+
+    void onRead(NimBLEDescriptor* pDescriptor) {
+        Serial.print(pDescriptor->getUUID().toString().c_str());
+        Serial.println(" Descriptor read");
+    };
+};
+
+
+//Define callback instances globally to use for multiple Charateristics/Descriptors 
+static DescriptorCallbacks dscCallbacks;
+static CharacteristicCallbacks chrCallbacks;
+
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Starting NimBLE Server");
+
+    /** sets device name */
+    NimBLEDevice::init("kath");
+
+    NimBLEDevice::setPower(ESP_PWR_LVL_P9); 
+    
+    pServer = NimBLEDevice::createServer();
+    pServer->setCallbacks(new ServerCallbacks());
+
+    NimBLEService* pLockService = pServer->createService("6eb697ea-1010-11eb-adc1-0242ac120002");
+    NimBLECharacteristic* pLockCharacteristic = pLockService->createCharacteristic(
+                                               "b4be2db8-100e-11eb-adc1-0242ac120002",
+                                               NIMBLE_PROPERTY::READ |
+                                               NIMBLE_PROPERTY::WRITE
+                                              );
+  
+    pLockCharacteristic->setValue("lock_unlock");
+    pLockCharacteristic->setCallbacks(&chrCallbacks);
+
+    /** 2904 descriptors are a special case, when createDescriptor is called with
+     *  0x2904 a NimBLE2904 class is created with the correct properties and sizes.
+     *  However we must cast the returned reference to the correct type as the method
+     *  only returns a pointer to the base NimBLEDescriptor class.
+     */
+    NimBLE2904* pLock2904 = (NimBLE2904*)pLockCharacteristic->createDescriptor("987125b0-100f-11eb-adc1-0242ac120002"); 
+    pLock2904->setFormat(NimBLE2904::FORMAT_UTF8);
+    pLock2904->setCallbacks(&dscCallbacks);
+  
+
+
+    /** Start the services when finished creating all Characteristics and Descriptors */  
+    pLockService->start();
+
+    NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+    /** Add the services to the advertisment data **/
+    pAdvertising->addServiceUUID(pLockService->getUUID());
+    /** If your device is battery powered you may consider setting scan response
+     *  to false as it will extend battery life at the expense of less data sent.
+     */
+    pAdvertising->setScanResponse(true);
+    pAdvertising->start();
+
+    Serial.println("Advertising Started");
+}
+
+
+void loop() {
+  delay(2000);
+}
